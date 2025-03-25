ce2126d7b08af51052ebef2f50f3cb07f9080fea
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-25 16:39:22 +0100

    Add technology IDs and update Main component for improved rendering

--- data/techs.js
+++ data/techs.js
@@ -1,25 +1,31 @@
 const technologies = [
     {
+        id: 1,
         title: "HTML",
         description: "The foundation of web content, HTML provides the basic structure of sites. It's the standard markup language that organizes content into elements like headings, paragraphs, and images."
     },
     {
+        id: 2,
         title: "CSS",
         description: "Cascading Style Sheets control the visual presentation of HTML elements. It handles layout, colors, fonts, spacing, and responsiveness, making websites aesthetically pleasing and user-friendly."
     },
     {
+        id: 3,
         title: "JavaScript",
         description: "A versatile programming language that adds interactivity to websites. It enables dynamic content updates, animated elements, form validation, and handles user interactions in real-time."
     },
     {
+        id: 4,
         title: "Node.js",
         description: "A runtime environment that executes JavaScript code outside browsers. It enables server-side scripting, building scalable network applications, and manages package dependencies through npm."
     },
     {
+        id: 5,
         title: "Express",
         description: "A minimal and flexible Node.js web application framework that provides robust features for web and mobile applications. It simplifies routing, middleware implementation, and API development."
     },
     {
+        id: 6,
         title: "ReactJS",
         description: "A JavaScript library for building user interfaces with reusable components. It efficiently updates and renders components, manages application state, and creates interactive UIs efficiently."
     }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import technologies from '../../data/techs.js'
 
 export default function Main() {
@@ -5,14 +7,14 @@ export default function Main() {
         <main id="main">
             <div id="buttons" className="d-flex row-cols-6 m-5 gap-3">
                 {
-                    technologies.map(tech => 
+                    technologies.map(tech =>
                         <div className="btn bg-primary text-white col">{tech.title}</div>
                     )
-                }  
+                }
             </div>
 
             <div className="card m-5 p-5">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, ut eius.
+                Nessun linguaggio selezionato
             </div>
         </main>
     )
