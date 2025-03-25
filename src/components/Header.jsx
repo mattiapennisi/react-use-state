ef205378b44ecaed0b532871bc3cbaf2fef627bd
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-25 16:36:29 +0100

    add technology buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header id="header" className="bg-secondary-subtle p-4 fs-2 text-left fw-semibold">
+        <header id="header" className="bg-secondary-subtle p-4 fs-4 text-left fw-semibold">
             Learn Web Development
         </header>
     )
