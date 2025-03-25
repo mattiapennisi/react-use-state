a917925e174d2786effb1fe530938962318fbf04
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-25 17:10:10 +0100

    edit Main component with technology selection functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <header id="header" className="bg-secondary-subtle p-4 fs-4 text-left fw-semibold">
-            Learn Web Development
+        <header id="header" className="bg-secondary-subtle p-4">
+            <h1 className="fs-4 fw-semibold">Learn Web Development</h1>
         </header>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,18 +3,29 @@ import { useState } from 'react'
 import technologies from '../../data/techs.js'
 
 export default function Main() {
+    const [buttonId, setButtonId] = useState(0)
+
+    const selectedTech = technologies.find(tech => tech.id === buttonId)
+
     return (
         <main id="main">
-            <div id="buttons" className="d-flex row-cols-6 m-5 gap-3">
+            <div id="buttons" className="d-flex m-5 gap-3">
                 {
                     technologies.map(tech =>
-                        <div className="btn bg-primary text-white col">{tech.title}</div>
+                        <div
+                            key={tech.id}
+                            className="btn bg-primary text-white"
+                            onClick={() => setButtonId(tech.id)}
+                        >
+                            {tech.title}
+                        </div>
                     )
                 }
             </div>
 
             <div className="card m-5 p-5">
-                Nessun linguaggio selezionato
+                <h2 className='mb-4 fw-bold fs-3'>{selectedTech ? selectedTech.title : ''}</h2>
+                <p className='fs-5'>{selectedTech ? selectedTech.description : 'Nessun linguaggio selezionato'}</p>
             </div>
         </main>
     )
